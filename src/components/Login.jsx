2a02627ce9867bc54f8da7f715ec62c19eb95084
Paul Gildehaus <pegildehaus@gmail.com>
2025-04-23 14:23:02 -0400

    Added an alert import from mui

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,7 +8,8 @@ import {
   Button,
   Box,
   Divider,
-  CircularProgress
+  CircularProgress,
+  Alert
 } from '@mui/material';
 import { Google as GoogleIcon } from '@mui/icons-material';
 import { authStyles } from '../styles/styles';
